e06fb7378c4c120c90f623e53fe910167ebf8479
TheTezma <chrisrichardsdev@gmail.cm>
2017-01-21 23:55:35 +1000

    Server Pinger

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="ping.php" method="POST">
+		<input type="text" name="ip" placeholder="ip">
+		<input type="text" name="port" placeholder="port">
+		<input type="submit" name="submit" value="Ping">
+	</form>
+</body>
+</html>
